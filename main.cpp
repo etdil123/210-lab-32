f1ece222ec49cb286dd8238335486bf221263bb6
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-11-15 20:27:30 -0800

    10 min commit

--- main.cpp
+++ main.cpp
@@ -9,10 +9,16 @@ void displayDeque(deque<Car>, int);
 
 void displayDeque(deque<Car> carDeque, int size) {
 
-    // for each element in deque - using Car print method 
-    for (int i = 0; i < size; i++) {
+    if (carDeque.empty() == true) {
         cout << "   ";
-        carDeque[i].print();
+        cout << "Empty";
+    }
+    else {
+        // for each element in deque - using Car print method 
+        for (int i = 0; i < size; i++) {
+            cout << "   ";
+            carDeque[i].print();
+        }
     }
 }
 
@@ -27,19 +33,37 @@ int main() {
     // add the INITIAL constant number of cars to deque 
     for (int i = 0; i < INITIAL; i++) {
         Car tempCar;
-        carDeque.push_front(tempCar);
+        carDeque.push_back(tempCar);
     }
     // display initial queue
     cout << "Initial queue:" << endl;
     displayDeque(carDeque, carDeque.size());
 
+    int counter = 0;
+
     while (carDeque.empty() == false) {
 
-        int lineOperation = 
+        // create random operation between 0 and 100
+        int lineOperation = (rand() % 100);
 
-
-
+        // if random number less than or equal to 55 --> car leaves toll both
+        if (lineOperation <= 55) {
+            cout << "Time: " << counter + 1 << " Operation: ";
+            cout << "Car paid: ";
+            carDeque.front().print();
+            carDeque.pop_front();
+        }
+        // else --> car joined the lane
+        else {
+            Car tempCar;
+            cout << "Time: " << counter + 1 << " Operation: ";
+            cout << "Joined lane: ";
+            tempCar.print();
+            carDeque.push_back(tempCar);
+        }
     }
+
+
 
     return 0;
 }
