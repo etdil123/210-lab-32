b29f3ec74b0761fc4bf65a75ee1c9b36d64b1f1e
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-11-15 20:02:24 -0800

    10 min commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+void displayDeque(deque<Car>, int);
+
+void displayDeque(deque<Car> carDeque, int size) {
+
+    // for each element in deque - using print method 
+    for (int i = 0; i < size; i++) {
+        cout << "   ";
+        carDeque[i].print();
+    }
+}
+
 int main() {
     // constant to store starting # of cars
     int INITIAL = 2;
@@ -11,7 +22,14 @@ int main() {
     // create a deque to store cars
     deque<Car> carDeque;
 
-    
+    // add the INITIAL constant number of cars to deque 
+    for (int i = 0; i < INITIAL; i++) {
+        Car tempCar;
+        carDeque.push_front(tempCar);
+    }
+
+    cout << "Initial queue:" << endl;
+    displayDeque(carDeque, carDeque.size());
 
     return 0;
 }
