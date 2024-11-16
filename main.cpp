22bf2b0db02582b62088844e1002b385e4af60e3
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-11-15 19:54:17 -0800

    10 min commit

--- main.cpp
+++ main.cpp
@@ -1,2 +1,17 @@
 #include "Car-1.h"
 #include <deque>
+
+using namespace std;
+
+int main() {
+    // constant to store starting # of cars
+    int INITIAL = 2;
+    srand(time(0));
+
+    // create a deque to store cars
+    deque<Car> carDeque;
+
+    
+
+    return 0;
+}
